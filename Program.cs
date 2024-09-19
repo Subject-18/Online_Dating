4234dc89290c149c246cedbd667ad22cfa68f72c
NAMAN SHARMA <95184688+Subject-18@users.noreply.github.com>
2024-09-19 21:22:21 +0530

    adding Ng

--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(); // for adding the cors from server side
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200")); // generally put this middleware before the mapcontrollers and the run function
 
 app.MapControllers();
 
