0375f76af838a2cc0add5808bde6f8d8f5a64761
NAMAN SHARMA <95184688+Subject-18@users.noreply.github.com>
2024-09-30 16:12:44 +0530

    ;)

--- Controllers/AcountController.cs
+++ Controllers/AcountController.cs
@@ -2,26 +2,35 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AcountController(DataContext context) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(string username, string password) // api endpoint for registering
+    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO) // api endpoint for registering
     {
+        if (await UserExists(registerDTO.Username))
+            return BadRequest("Username already taken");
         using var hmac = new HMACSHA512();
 
         var user = new AppUser{
-             UserName = username,
-              PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+             UserName = registerDTO.Username.ToLower(),
+              PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
               PasswordSalt = hmac.Key
              };
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
     }
 }
